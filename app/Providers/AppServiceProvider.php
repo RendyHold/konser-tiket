3e5906c370a21ae5ee0f01413aa61ee9e7f323e6
RendyHold <srendy1717@gmail.com>
2025-09-04 00:54:30 +0700

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        // pakai env agar fleksibel
+        if (app()->environment('production') && env('FORCE_HTTPS', true)) {
             URL::forceScheme('https');
         }
     }
