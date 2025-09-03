1099e645aadf07f1fc11ec877fa3f0d3b7133f82
RendyHold <srendy1717@gmail.com>
2025-09-04 00:35:22 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\DashboardController;
 Route::view('/', 'welcome')->name('home');
 
 /* Dashboard (jangan dua kali) */
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 });
