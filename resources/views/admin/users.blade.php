b217decfcb45fb60361148652e9d31e35833a29a
RendyHold <srendy1717@gmail.com>
2025-09-06 18:02:02 +0700

    a

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -111,7 +111,7 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
-                <form method="POST" action="{{ route('admin.users.resetPassword', $u) }}">
+                <form method="POST" action="{{ route('users.resetPassword', $u) }}">
                     @csrf
                     @method('POST')
 
