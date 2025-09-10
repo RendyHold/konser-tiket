f5e64af243da13ea50a84d01a20ee27bd8d7a6c3
RendyHold <srendy1717@gmail.com>
2025-09-11 03:54:09 +0700

    Update show.blade.php

--- resources/views/ticket/show.blade.php
+++ resources/views/ticket/show.blade.php
@@ -4,7 +4,7 @@
 <div class="container mx-auto max-w-3xl p-4">
   <h1 class="text-2xl font-bold mb-4">Tiket Saya</h1>
 
-  @if($tickets && $tickets->isEmpty())
+  @if($tickets->isEmpty())
     <p class="text-gray-600">Belum ada tiket.</p>
 @else
     <table class="w-full border">
@@ -18,22 +18,22 @@
         </thead>
         <tbody>
             @foreach($tickets as $t)
-                <tr>
-                    <td class="p-2 border align-top">
-                        <div class="font-mono">{{ $t->code }}</div>
-                        @isset($t->npm)
-                            <div class="text-xs text-gray-500">NPM: {{ $t->npm }}</div>
-                        @endisset
-                    </td>
-                    <td class="p-2 border">
-                        {{-- Menampilkan barcode --}}
-                        <img src="{{ url('/barcodes/'.$t->code.'_barcode.png') }}" alt="Barcode" />
-                    </td>
-                    <td class="p-2 border align-top">{{ $t->status ?? '-' }}</td>
-                    <td class="p-2 border align-top">
-                        {{ $t->scanned_at ? $t->scanned_at->format('d M Y H:i') : '-' }}
-                    </td>
-                </tr>
+            <tr>
+                <td class="p-2 border align-top">
+                    <div class="font-mono">{{ $t->code }}</div>
+                    @isset($t->npm)
+                        <div class="text-xs text-gray-500">NPM: {{ $t->npm }}</div>
+                    @endisset
+                </td>
+                <td class="p-2 border">
+                    {{-- Menampilkan barcode --}}
+                    <img src="{{ url('/barcodes/'.$t->code.'_barcode.png') }}" alt="Barcode" />
+                </td>
+                <td class="p-2 border align-top">{{ $t->status ?? '-' }}</td>
+                <td class="p-2 border align-top">
+                    {{ $t->scanned_at ? $t->scanned_at->format('d M Y H:i') : '-' }}
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
