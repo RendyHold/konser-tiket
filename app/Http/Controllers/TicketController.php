07edf82383bbccb69c91c9e7ac16ae454bda4bcf
RendyHold <srendy1717@gmail.com>
2025-09-11 08:17:50 +0700

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -89,7 +89,7 @@ class TicketController extends Controller
         file_put_contents($qrCodePath, $qrCode);
 
         // Gabungkan Barcode dan QR Code dengan Gambar Tiket
-        $ticketImage = imagecreatefrompng(public_path('img/tiket.png')); // Gambar tiket awal
+        $ticketImage = imagecreatefrompng(public_path('image/tiket.png')); // Gambar tiket awal
         $barcodeImage = imagecreatefromstring($barcode); // Gambar barcode
         $qrCodeImage = imagecreatefromstring($qrCode); // Gambar QR Code
 
